5462ead5f6b981c5a454eb2e51038d8702b197c5
Antek97 <76130883+Antek97@users.noreply.github.com>
2021-01-08 23:50:48 +0100

    Menu create

--- src/menu.js
+++ src/menu.js
@@ -1,19 +1,61 @@
 import React from 'react';
 import AgeConfirmation from './ageConfirmation';
 
-class MenuHeader extends React.Component {
+class BeerList extends React.Component {
   state = {  }
   render() { 
     return (
+      <div>Hello</div>
+      );
+  }
+}
+
+class MenuHeader extends React.Component {
+  state = { 
+    Layer : ""
+   }
+
+
+  handleklickBeers = () =>{
+    this.setState({
+        Layer : "myBeer"
+    })
+    console.log(this.state.Layer)
+  }
+  whereBuy = () =>{
+    this.setState({
+        Layer : "whereBuy"
+    })
+    
+  }
+  myInfo = () =>{
+    this.setState({
+        Layer : "myInfo"
+    })
+    
+  }
+  contact = () =>{
+    this.setState({
+        Layer : "contact"
+    })
+    
+  }
+
+  render() { 
+    return (
       <> 
-        
         <div>
           <div>Hello123</div>
-          <p>Bedzie komponent</p>
-          <p>Bedzie komponent</p>
-          <p>Bedzie komponent</p>
-          <p>Bedzie komponent</p>
+          <ul>
+          <li>Strona główna</li>
+          <li onClick={this.handleklickBeers}>Nasze piwa</li>
+          <li onClick={this.whereBuy}>Gdzie kupisz</li>
+          <li onClick={this.myInfo}>O nas</li>
+          <li onClick={this.contact}>Kontakt</li>
+          </ul>
         </div>
+        <button onClick={() => console.log(this.state.Layer)}>test</button>
+        <BeerList/>
       </>
      );
   }

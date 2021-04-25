cd5266bc0afd16c9624adce00f35f217f296031b
Antek97 <76130883+Antek97@users.noreply.github.com>
2021-04-25 22:37:04 +0200

    restoration beersList.js

---
+++ src/App/beersList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Beer from './beer';
+
+const BeersList = (props) => {
+  
+  const done = props.beers
+  done.sort((a,b) => b.id - a.id)
+  const beers = done.map(beer => <Beer key={beer.id} beer={beer} deleteBeer={props.deleteBeer}/>)
+ 
+ 
+  return ( 
+    <div>
+      {beers}
+    </div>
+  );
+}
+ 
+export default BeersList;

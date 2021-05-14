7397f8674cad598469d5bd42a6e2be30997016be
Antek97 <76130883+Antek97@users.noreply.github.com>
2021-05-14 22:46:32 +0200

    beersList.js  adding a search engine

--- src/App/beersList.js
+++ src/App/beersList.js
@@ -3,14 +3,16 @@ import Beer from './beer';
 
 const BeersList = ( props ) => {
   
+  const search = props.search
   const done = props.beers
+  
   done.sort((a,b) => b.id - a.id)
   const beers = done.map(beer => <Beer key={beer.id} beer={beer} deleteBeer={props.deleteBeer}/>)
- 
- 
+  const found = beers.find(e => e.props.beer.beerName === search )
+
   return ( 
     <div>
-      {beers}
+      {search === '' ? beers : found}
     </div>
   );
 }

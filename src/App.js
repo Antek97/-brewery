d37d4ad9f295f14d75e2023f309cca6316e1efee
Antek97 <76130883+Antek97@users.noreply.github.com>
2021-05-16 21:59:30 +0200

    App.js created connection through hooks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
+import React, { PureComponent } from 'react';
+import { AppContext, defaultObiect } from './AppContext';
+
 import Menu from './App/menu';
 
-function App() {
-  return (
-  <Menu/>
-  );
-}
+class App extends PureComponent {
+  state = {
+    usserLogged: defaultObiect.usserLogged,
+    id: defaultObiect.id,
+    name: defaultObiect.name,
+   }
 
+  render() { 
+    return ( 
+      <AppContext.Provider value={{
+        test: this.state.test,
+        usserLogged: this.state.usserLogged,
+        id: this.state.id,
+        name: this.state.name,
+        }}>
+
+        <Menu/>
+
+      </AppContext.Provider>
+     );
+  }
+}
+ 
 export default App;
+
+
+// function App() {
+//   return (
+//     <AppContext.Provider value={{
+//       test: this.state.test,
+//       usserLogged: this.state.usserLogged,
+//       id: this.state.id,
+//       name: this.state.name,
+//     }}>
+
+//       <Menu/>
+//     </AppContext.Provider>
+//   );
+// }
+
+// export default App;
+

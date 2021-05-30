1fd981e67eed9a5d3dfbf863e8f6c15d221a52fb
Antek97 <76130883+Antek97@users.noreply.github.com>
2021-05-30 18:52:53 +0200

    removing the slider function

--- front-end/src/App/main/descriptionApp.js
+++ front-end/src/App/main/descriptionApp.js
@@ -2,8 +2,6 @@ import React from 'react';
 
 import { SiInvision, SiGithub, SiGmail } from "react-icons/si";
 
-import Slider from './slider';
-
 const descriptionAppTitle = <h1 className="MainPageContainer__VlogTitle">Hello there</h1>;
 const descriptionApp = <p className="myInfo">Welcome to my beer application.
 My name is Sebastian and I am the creator of an application for recording beers that you have encountered or made and evaluating them, thanks to it you do not have to write them in a notebook.</p>;
@@ -31,7 +29,6 @@ const DescriptionApp =()=> {
           {descriptionAppTitle}
           {descriptionApp}
         </header>
-          <Slider/>
           <footer>
             {articleFirst}
             {contact}
